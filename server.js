ad3f781906258e9167d762db0eb1b3bd1d4b5057
blockchainbuddha@gmail.com <blockchainbuddha@gmail.com>
2018-06-11 22:24:44 -0400

    test server

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello Node.js!\n');
-    if ('host' in req.headers) {
-        var host = req.headers['host'];
-        res.write('Vhost is ' + host.split(':')[0] + '\n');
+var http = require('http'),
+    pg = require('pg');
+ 
+var pg_url = "tcp://hosting-db@localhost/postgres";
+var pg_conn = new pg.Client(pg_url);
+ 
+var test_pg_conn = function(callback) {
+  pg_conn.connect(function(err) {
+    if (err) {
+      console.log(err);
+      if (callback) callback("NOT WORKING");
     } else {
-        res.write('No vhost specified\n');
+      console.log('Connected to PostgreSQL');
+      if (callback) callback("OK");
     }
+  });
+};
+ 
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello Node.js!\n');
+  test_pg_conn(function(pg_status) {
+    res.write('PostgreSQL connection: ' + pg_status + '\n');
     res.end();
-}).listen(process.env.PORT);
+  });
+}).listen(process.env.PORT)
