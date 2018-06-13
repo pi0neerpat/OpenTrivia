a4e550665ddc228d16c7b7f22ac05a204ca42fa4
Patrick Gallagher <blockchainbuddha@gmail.com>
2018-06-12 21:42:40 -0400

    update to package.json via npm init, no longer specify node version

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 var port = process.env.PORT;
-connect().use(serveStatic(__dirname)).listen(port, function(){
-    console.log(port);
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log(8080);
 });
