b2ab7089213785e5456f5288e4efaa7a514baa79
Patrick Gallagher <blockchainbuddha@gmail.com>
2018-09-05 03:39:51 -0400

    removed prestart command, memory taking too long. including .next files now by changing .gitignore

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const { createServer } = require("http");
 const next = require("next");
 
+const port = process.env.PORT;
+
 const app = next({
   //specifies production or dev mode
   // Checks for this variable, if true, then production mode
@@ -12,7 +14,7 @@ const routes = require("./routes");
 const handler = routes.getRequestHandler(app);
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000, err => {
+  createServer(handler).listen(port || 3000, err => {
     if (err) throw err;
     console.log("Ready on localhost:3000");
   });
